75332ebac7607207114fdae7a5a29c519803d643
kimmimileehuirockandroll <fkr006@snu.ac.kr>
2026-06-15 22:29:02 +0900

    Update shared.py

--- shared.py
+++ shared.py
@@ -24,7 +24,6 @@ def apply_css():
     <style>
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
-    header {visibility: hidden;}
     [data-testid="stToolbar"] {visibility: hidden;}
     [data-testid="stDecoration"] {visibility: hidden;}
     </style>
